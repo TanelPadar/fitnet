0afec007cdf557f33391754eb3ff34f3b78c9ad7
tanelpadar <padar144@hotmail.com>
2023-04-10 17:06:24 +0300

    sidebar killed

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/main.css';
+
+
+
+const Sidebar = () => {
+    return (
+       <div className="container-fluid">
+           <div className="row">
+           <div className="col-2 vh-100 sidebar">
+               <nav className="d-flex flex-column mt-2">
+                   <a href="#">Tanel Padar</a>
+                   <a href="#">Clients</a>
+                   <a href="#">Schedule</a>
+                   <a href="#">Settings</a>
+
+               </nav>
+           </div>
+           <div className="col-10">
+
+           </div>
+           </div>
+       </div>
+    );
+};
+
+export default Sidebar;
