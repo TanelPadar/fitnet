12dabecdccab37e5a44ca54d411f1df03bdf98df
SanderPalk <95194970+SanderPalk@users.noreply.github.com>
2023-05-11 18:55:00 +0300

    Exercises set view

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -16,7 +16,6 @@ function Exercises(props) {
         try {
             const response = await axios.get(api + `/get-exercises/${WorkoutId}`)
             await setExercises(response.data)
-            console.log(exercises)
         } catch (e) {
             console.log(e)
         }
@@ -25,42 +24,32 @@ function Exercises(props) {
     return (
         <div>
             <h5 className="text-muted my-3">{ props.WorkoutDesc}</h5>
-            <table className="table mt-5">
+            <table className="table mt-5 w-50 center">
                 <thead>
                 <p className="fw-bold text-muted">Muscle Group:</p>
                 <tr>
                     <th scope="col">Row</th>
                     <th scope="col">Exercise</th>
-                    <th scope="col">Reps</th>
-                    <th scope="col">Sets</th>
-                    <th scope="col">Weight</th>
                 </tr>
                 </thead>
                 <tbody>
                 {exercises.map((exercise, index) => {
-                    let rowNumber = index * 2;
+                    let rowNumber = index + 1;
+                    const exerciseSets = exercise.sets
                     return (
                         <>
                             <tr key={exercise._id}>
                                 <th scope="row">{rowNumber}</th>
                                 <td>{exercise.exerciseName}</td>
-                                <td>{exercise.sets[0].reps}</td>
-                                <td>{exercise.sets[0].weight}</td>
-                                <td>{exercise.sets[0].reps}</td>
+                                <td></td>
                             </tr>
-                            {exercise.sets.length > 1 &&
-                                exercise.sets.slice(1).map((set, setIndex) => {
-                                    rowNumber++;
-                                    return (
-                                        <tr key={setIndex}>
-                                            <th scope="row">{rowNumber}</th>
-                                            <td>{exercise.exerciseName}</td>
-                                            <td>{set.reps}</td>
-                                            <td>{set.weight}</td>
-                                            <td>{set.reps}</td>
-                                        </tr>
-                                    );
-                                })}
+                            {exerciseSets.map((set) => (
+                                <tr>
+                                    <td></td>
+                                    <td>{set.weight} kg</td>
+                                    <td>{set.reps}</td>
+                                </tr>
+                                ))}
                         </>
                     );
                 })}
